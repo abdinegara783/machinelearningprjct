37a841f2160ed0aab2da1917e185103007eae749
ABDI NEGARA GUCI <102046045+abdinegara783@users.noreply.github.com>
2022-10-17 12:45:46 +0700

    mendeteksi kolom null(missing value)

--- BDC.py
+++ BDC.py
@@ -3,3 +3,6 @@ dataku=dataku.copy()
 #Melihat dimensi data
 dataku.shape
 #melihat variabel null(missing value)
+# Mendeteksi kolom yang memiliki data NA
+kolom_na = [kolom for kolom in dataku.columns if dataku[kolom].isnull().sum() > 0]
+kolom_na
