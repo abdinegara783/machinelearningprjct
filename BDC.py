76f11ba1368c9c56d5f9e5c9dcfb30a9917ad8b1
vikyiqbal <85992710+vikyiqbal@users.noreply.github.com>
2022-10-17 12:46:47 +0700

    Mereduksi dimensi data

--- BDC.py
+++ BDC.py
@@ -6,3 +6,5 @@ dataku.shape
 # Mendeteksi kolom yang memiliki data NA
 kolom_na = [kolom for kolom in dataku.columns if dataku[kolom].isnull().sum() > 0]
 kolom_na
+#Mereduksi Dimensi Data
+dataku.drop(dataku.columns[[0,1,2,3,4,5]], axis=1, inplace=True)
